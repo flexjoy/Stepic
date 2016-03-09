4dfba90ebe8ebd0e2617c1d050b4b5dfcc83dec4
Sergey Cherepanov <flexjoy@ya.ru>
2016-03-09 15:54:00 +0300

    Main.java: add in application entry point
    Create servlet and start jetty server.

---
+++ L1.1/src/main/java/main/Main.java
@@ -0,0 +1,26 @@
+package main;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.MirrorRequestServlet;
+
+/**
+ * @author Sergey Cherepanov
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        MirrorRequestServlet mirrorRequestServlet = new MirrorRequestServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(mirrorRequestServlet), "/mirror");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        System.out.println("Server started");
+        server.join();
+    }
+}
