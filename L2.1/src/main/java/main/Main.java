04ef19d14e0c0081ff1cb5511b032c8835b40b26
flexjoy <flexjoy@ya.ru>
2016-03-14 22:04:35 +0300

    add AccountService and UserProfile classes

---
+++ L2.1/src/main/java/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import accounts.AccountService;
+
+/**
+ * @author Sergey Cherepanov on 14.03.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        AccountService accountService = new AccountService();
+    }
+}
